9772fc100bf9d3b5558041a605bd8463c0f0508b
Evan Astle <starphys@gmail.com>
2022-05-30 12:50:48 -0700

    Added express server for exposing metrics Right printer metrics collected and displayed

---
+++ printer/metrics.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const util = require('./util.js');
+const { inkLevel } = util;
+const config = require('../config/config.json');
+const { RIGHT_PRINTER_IP } = config;
+
+const client = require('prom-client');
+
+let register = new client.Registry();
+
+const inkLevelRight = new client.Gauge({
+  name: 'ink_level_right',
+  help: 'metric_help',
+  async collect() {
+    const currentValue = await inkLevel(RIGHT_PRINTER_IP);
+    this.set(currentValue);
+  },
+});
+
+register.registerMetric(inkLevelRight);
+
+register.setDefaultLabels({
+  app: 'sce-printer',
+});
+
+client.collectDefaultMetrics({ register });
+
+app.get('/metrics', async (request, response) => {
+  response.setHeader('Content-type', register.contentType);
+  response.end(await register.metrics());
+});
+
+app.listen(5000, () => {});
