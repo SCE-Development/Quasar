1630c742f80042ce179a5c7f6770ddade35fd5f6
Evan Astle <starphys@gmail.com>
2022-05-30 12:55:27 -0700

    Start metrics server when starting print server

--- printer/what.sh
+++ printer/what.sh
@@ -28,7 +28,12 @@ sleep 10
 create_and_enable_printer $LEFT_PRINTER_NAME $LEFT_PRINTER_IP
 create_and_enable_printer $RIGHT_PRINTER_NAME $RIGHT_PRINTER_IP
 
+echo Starting metrics server...
+
+# Start express server to expose metrics
+node printer/metrics.js
+
 echo Starting print server...
 
 # Run the actual code to read from SQS/S3 and send files to the printers.
-node printer/PrintHandler.js
+node printer/PrintHandler.js 
