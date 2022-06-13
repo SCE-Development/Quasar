5d68820d6e491a97fbbaba853b6567a8ba9618b6
Arjun <ccldarjun@icloud.com>
2022-06-12 22:19:57 -0700

    lets do clever stuff like this when we actually build out communication with printers
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- tunnel/app.py
+++ tunnel/app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def api():
-    return (json.dumps(
-            {"left" : {"up" : True, "inkLevel" : 60},
-             "right": {"up" : True, "inkLevel" : 70}}),
-            200,
-            {'Content-Type' : 'application/json'})
+    return "Hello from Quasar!"
         
 
 if __name__ == "__main__":
