32aef7cae6d64b1436a2cbf4cd3e8346512fb6cb
Akshit Sharma <akshitsharmaa65@gmail.com>
2022-06-15 15:55:35 -0700

    fixed merge conflicts

--- ledsign/LEDSignHandler.js
+++ ledsign/LEDSignHandler.js
@@ -26,8 +26,7 @@ setInterval(async () => {
   if (!data) {
     return;
   }
-  headCount.inc(1);
-  response.json({headCount, headCount});
+  
   await axios.post(LED_URL + 'api/update-sign', data.Body);
   const deleteParams = {
     QueueUrl: queueUrl,
