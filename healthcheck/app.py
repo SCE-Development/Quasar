c40125bfddac98e9730c3fb2ca24b3e570f1a36d
Arjun <ccldarjun@icloud.com>
2022-07-08 17:28:45 -0700

    Healthcheck for ledsign (#62)
    * led status in healthcheck
    
    * forward ledsign's healthcheck through quasar
    
    * use urljoin and give richer error if quasar cannot reach ledsign

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 import json
 import sys
+from urllib.parse import urljoin
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/healthcheck/printer")
 def api():
-    return "Hello from Quasar!"
-        
+    return "printer is up!"
+
+@app.route("/healthcheck/ledsign")
+def ledsign_health_api():
+    with open("/app/config/config.json", "r") as file:
+        led_url = json.load(file)["LED_URL"]
+
+    url = urljoin(led_url, "/api/health-check")
+
+    try:
+        resp = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        return jsonify({"success" : False})
+    if resp.status_code >= 400:
+        return jsonify({"success" : False, "ledsign" :
+            {"status_code": resp.status_code, "text": resp.text}})
+
+    return (resp.text, resp.status_code, resp.headers.items())
+
 
 if __name__ == "__main__":
     port = 14000
