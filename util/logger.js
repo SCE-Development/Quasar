1f3815e13d83a542c7443be5d318b8ae277433dc
Praveen Kandasamy <pravee.samy@gmail.com>
2022-07-08 19:56:19 -0700

    if statement for windows or mac

--- util/logger.js
+++ util/logger.js
@@ -5,6 +5,9 @@ const consoleColors = {
   yellow: '\x1b[33m',
   default: '\x1b[0m',
 };
+
+const { win32 } = require('path');
+const util = require('util');
   
 const LOG_LEVELS = {
   DEBUG: 'DEBUG',
@@ -17,9 +20,13 @@ function getLineOfCode() {
   const e = new Error();
   // point out that 2 is for parent, 3 for grandparent etc...
   // yields "at Object.<anonymous> (/path/to/file.js:7:1)"
+  const backslash = '/';
+  if(process.platform === 'win32'){
+    backslash = '\\';
+  }
   const parentFunc = e.stack.split('\n')[4];
   const fileAndLineOfCode = parentFunc
-    .split('/') // yields "file.js:7:1)"
+    .split(backslash) // yields "file.js:7:1)"
     .reverse()[0] // yields "file.js:7:1"
     .split(')')[0]; // yields "file.js:7:1"
   return fileAndLineOfCode;
@@ -27,11 +34,12 @@ function getLineOfCode() {
   
 // Do not call this function directly! Use the below factory
 // functions instead
-function printToConsole(level, message) {
+function printToConsole(level, ...message) {
   let formattedMessage = new Date().toISOString();
+  const args = util.format(...message);
   formattedMessage += ` ${getLineOfCode()}`;
   formattedMessage += ` ${level}`;
-  formattedMessage += ` ${message}`;
+  formattedMessage += ` ${args}`;
   // Print the log as red or green if the level is error or warning
   if (level === LOG_LEVELS.ERROR) {
     formattedMessage = `${consoleColors.red}${formattedMessage}${consoleColors.default}`;
@@ -41,20 +49,20 @@ function printToConsole(level, message) {
   console.log(formattedMessage);
 }
   
-function debug(message) {
-  printToConsole(LOG_LEVELS.DEBUG, message);
+function debug(...message) {
+  printToConsole(LOG_LEVELS.DEBUG, ...message);
 }
   
-function info(message) {
-  printToConsole(LOG_LEVELS.INFO, message);
+function info(...message) {
+  printToConsole(LOG_LEVELS.INFO, ...message);
 }
   
-function warn(message) {
-  printToConsole(LOG_LEVELS.WARNING, `${message}`);
+function warn(...message) {
+  printToConsole(LOG_LEVELS.WARNING, ...message);
 }
   
-function error(message) {
-  printToConsole(LOG_LEVELS.ERROR, `${message}`);
+function error(...message) {
+  printToConsole(LOG_LEVELS.ERROR, ...message);
 }
   
 module.exports = { debug, info, warn, error };
