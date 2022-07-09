00388f7bcd915f676d754ab84ac4d736a560c6ad
CCLDArjun <ccldarjun@icloud.com>
2022-07-08 17:13:59 -0700

    use urljoin and give richer error if quasar cannot reach ledsign

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 import requests
 import json
 import sys
+from urllib.parse import urljoin
 
 app = Flask(__name__)
 
@@ -14,13 +15,18 @@ def ledsign_health_api():
     with open("/app/config/config.json", "r") as file:
         led_url = json.load(file)["LED_URL"]
 
+    url = urljoin(led_url, "/api/health-check")
+
     try:
-        resp = requests.get(led_url + "/api/health-check")
+        resp = requests.get(url)
     except requests.exceptions.ConnectionError:
         return jsonify({"success" : False})
+    if resp.status_code >= 400:
+        return jsonify({"success" : False, "ledsign" :
+            {"status_code": resp.status_code, "text": resp.text}})
 
     return (resp.text, resp.status_code, resp.headers.items())
-        
+
 
 if __name__ == "__main__":
     port = 14000
