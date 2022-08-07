68353b16d37644df65ebdc66b48bfa030e9f5393
Truong Phu Vu <truongphu2842@gmail.com>
2022-08-07 13:41:58 -0700

    created function to support printing from Discord (#80)
    * merge dev
    
    * merge dev
    
    * add logging to exec function
    
    * lint
    
    * Update printer/PrintHandler.js
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    Co-authored-by: Neel Patel <np.patelneel@gmail.com>
    Co-authored-by: Neel Patel <75186517+neelp03@users.noreply.github.com>
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- printer/PrintHandler.js
+++ printer/PrintHandler.js
@@ -4,6 +4,7 @@ const {
   PRINTING
 } = require('../config/config.json');
 const awsSDK = require('aws-sdk');
+const axios = require('axios');
 const fs = require('fs');
 const s3 = new awsSDK.S3({ apiVersion: '2012-11-05' });
 const creds = new awsSDK.Credentials(AWS.ACCESS_ID, AWS.SECRET_KEY);
@@ -26,6 +27,41 @@ const params = {
   VisibilityTimeout: 0,
   WaitTimeSeconds: 0,
 };
+
+
+/**
+ * Download PDF file from URL.
+ * @param {Sring} url : PDF file url
+ * @returns Promise return the path to downloaded PDF file
+ * if it was successfully download. It then returns false if there
+ * was an error.
+ */
+async function downloadFileFromURL(url) {
+  return new Promise( ( resolve ) => {
+    axios({
+      url: url,
+      method: 'GET',
+      responseType: 'stream'
+    })
+      .then(response => {
+        // discord attachment URLs look like below, where id and name
+        // are unique to the uploaded attachment
+        // https://cdn.discordapp.com/attachments/<server>/<id>/<name>
+        const attachmentId = url.split('/')[5];
+        const fileName = `/tmp/${attachmentId}.pdf`;
+        const filePath = `/tmp/${fileName}.pdf`;
+        fs.writeFileSync(filePath, '');
+        const fileStream = fs.createWriteStream(filePath);
+        response.data.pipe(fileStream);
+        logger.info('Successfully downloaded file from url to', filePath);
+        resolve(filePath);
+      })
+      .catch(error => {
+        logger.error('Unable to download file from URL:', error);
+        resolve(false);
+      });
+  });
+}
 
 function deleteFile(fileNo) {
   const objectToDelete = {
@@ -88,36 +124,46 @@ async function downloadFileFromS3(fileNo) {
  
 setInterval(async () => {
   const data = await readMessageFromSqs(params, sqs);
-  if (!data) {
-    return;
-  }
-  const { fileNo, copies, pageRanges } = data.Body;
-  const pages = pageRanges === 'NA' ? '' : '-P ' + pageRanges;
-  const path = `/tmp/${fileNo}.pdf`;
- 
-  const dataFromS3 = await downloadFileFromS3(fileNo);
-  if (!dataFromS3) {
-    logger.warn('Unable to download file, skipping it');
-    return;
-  }
-  fs.writeFileSync(path, dataFromS3.Body, 'binary');
+  if (!data) return;
+
+  // Checking if there is a file URL sent to SQS from Discord.
+  const fromDiscord = (data.Body.fileURL) ? true : false;
   const printer = determinePrinterForJob();
+
+  const { fileNo, copies = 1, pages = 'NA'} = data.Body;
+  
+  let filePath = `/tmp/${fileNo}.pdf`;
+ 
+  if(!fromDiscord) {
+    const dataFromS3 = await downloadFileFromS3(fileNo);
+    if (!dataFromS3) {
+      logger.warn('Unable to download file, skipping it');
+      return;
+    }
+    fs.writeFileSync(filePath, dataFromS3.Body, 'binary');
+    // delete file from S3
+    deleteFile(fileNo);
+  } else {
+    const fileDownloaded = await downloadFileFromURL(data.Body.fileURL);
+    if(!fileDownloaded) return;
+    filePath = fileDownloaded.pdfFilePath;
+  }
   exec(
     `lp -n ${copies} ${pages} -o sides=one-sided -d ` +
-      `HP-LaserJet-p2015dn-${printer} ${path}`,
+    `HP-LaserJet-p2015dn-${printer} ${filePath}`,
     (error, stdout, stderr) => {
       if(error) logger.error('exec returned error:', error);
       if(stderr) logger.error('exec returned stderr:', stderr);
-      exec(`rm ${path}`, () => { });
+      exec(`rm ${filePath}`, () => { });
+
+      // delete from sqs
       const deleteParams = {
         QueueUrl: queueUrl,
         ReceiptHandle: data.ReceiptHandle,
       };
-      deleteFile(fileNo);
       sqs.deleteMessage(deleteParams, (err) => {
         if (err) logger.error('unable to delete message from SQS', err);
       });
     });
 }, 10000);
  
-
