85126130cf15496880e5ee31c4180a55cbad7c52
Cool Guy Awal <63530023+akshtshrma24@users.noreply.github.com>
2022-08-24 20:32:59 -0700

    lets remove the led sign from quasar now (#102)
    * lets remove the led sign from quasar now
    
    * lets remove the led sign from quasar now
    
    Co-authored-by: evan ugarte <evanuxd@gmail.com>

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -10,24 +10,6 @@ app = Flask(__name__)
 def api():
     return "printer is up!"
 
-@app.route("/healthcheck/ledsign")
-def ledsign_health_api():
-    with open("/app/config/config.json", "r") as file:
-        led_url = json.load(file)["LED_URL"]
-
-    url = urljoin(led_url, "/api/health-check")
-
-    try:
-        resp = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        return jsonify({"success" : False})
-    if resp.status_code >= 400:
-        return jsonify({"success" : False, "ledsign" :
-            {"status_code": resp.status_code, "text": resp.text}})
-
-    return (resp.text, resp.status_code, resp.headers.items())
-
-
 if __name__ == "__main__":
     port = 14000
     if len(sys.argv) >= 2:
