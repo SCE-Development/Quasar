71f5028252a171df0ec8be6389ac0b2f67ec6ec6
Trung Tran <trungtran63470@gmail.com>
2024-07-07 23:56:01 -0700

    remove pageRanges

--- printer/server.py
+++ printer/server.py
@@ -120,14 +120,13 @@ def metrics():
 
 
 @app.post("/print")
-async def read_item(file: UploadFile = File(...), copies: str = Form(...), sides: str = Form(...), pageRanges: str = Form(...)):
+async def read_item(file: UploadFile = File(...), copies: str = Form(...), sides: str = Form(...)):
     """
     incoming request to print looks like
     {
       "file": file data
       "copies": integer or whatever, we insert this into the lp command,
       "sides": string value from user input on clark frontend; we insert this into the lp command,
-      "pageRanges": string value from user input on clark frontend; we insert this into the lp command,
     }
     """
     try:
@@ -139,7 +138,6 @@ async def read_item(file: UploadFile = File(...), copies: str = Form(...), sides
         send_file_to_printer(
             str(file_path),
             copies,
-            page_range=pageRanges,
             sides=sides,
         )
         pathlib.Path(file_path).unlink()
