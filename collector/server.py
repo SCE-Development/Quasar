e991af04731dc36c23db05dc8c80e0b8bfcc2da8
weslayer <wesleywong1245@yahoo.com>
2024-07-19 17:51:12 -0700

    fix comments from evan

--- collector/server.py
+++ collector/server.py
@@ -57,8 +57,6 @@ class SnmpOid(enum.Enum):
 
 def get_snmp_data(ip):
     while True:
-        ink_level = 0
-        ink_cap = 0
         for oid in SnmpOid:
             with snmp_req_duration.time():
                 errorIndication, errorStatus, errorIndex, varBinds = next(
