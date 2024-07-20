801b97f81afb701975001c4a216f52c91638a408
weslayer <wesleywong1245@yahoo.com>
2024-07-19 19:29:35 -0700

    SNMP Collector (#113)
    * first commit for collector snmp thing
    
    * remove grafana and prometheus yml
    
    * yml no more port
    
    * dockerfile thign
    
    * full dir for dockerfile
    
    * add door and tray status to prometheus metrics
    
    * Form data (#112)
    
    * change /print to handle formData
    
    * added side parameter
    
    * remove unused annotated type
    
    * remove pageRanges
    
    * add port
    
    * remove ports
    
    * Update docker-compose.yml
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * Update printer/requirements.txt
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
    
    * code refactor with cool prometheus labels
    
    * set error values for door and tray status to 1
    
    * validate metrics with grafana, add ink_cap to avoid divide by zero
    
    * is_error code logic mind kaboom
    
    * fix code
    
    * fix code again lol
    
    * fix silly
    
    * is_error=false no more
    
    * more fix
    
    * fix finally maybe
    
    * add default value to error
    
    * add back while loop
    
    * put while loop in the correct spot
    
    * summary for time
    
    * time
    
    * clean up
    
    * fix comments from evan
    
    * first commit for collector snmp thing
    
    * remove grafana and prometheus yml
    
    * yml no more port
    
    * dockerfile thign
    
    * full dir for dockerfile
    
    * add door and tray status to prometheus metrics
    
    * code refactor with cool prometheus labels
    
    * set error values for door and tray status to 1
    
    * validate metrics with grafana, add ink_cap to avoid divide by zero
    
    * is_error code logic mind kaboom
    
    * fix code
    
    * fix code again lol
    
    * fix silly
    
    * is_error=false no more
    
    * more fix
    
    * fix finally maybe
    
    * add default value to error
    
    * add back while loop
    
    * put while loop in the correct spot
    
    * summary for time
    
    * time
    
    * clean up
    
    * fix comments from evan
    
    * first commit for collector snmp thing
    
    * remove grafana and prometheus yml
    
    * code refactor with cool prometheus labels
    
    * validate metrics with grafana, add ink_cap to avoid divide by zero
    
    * is_error code logic mind kaboom
    
    * fix code
    
    * is_error=false no more
    
    * fix finally maybe
    
    * add default value to error
    
    * put while loop in the correct spot
    
    * fix comments from evan
    
    * final
    
    ---------
    
    Co-authored-by: Trung Tran <48637790+trungtran1234@users.noreply.github.com>
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

---
+++ collector/server.py
@@ -0,0 +1,123 @@
+import time
+import argparse
+from threading import Thread
+import enum
+import logging
+
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+import prometheus_client
+from pysnmp.hlapi import *
+import uvicorn
+
+snmp_metric = prometheus_client.Gauge(
+    "snmp_metric",
+    "ex: Number of pages printed",
+    ["name"],
+)
+
+snmp_error = prometheus_client.Gauge(
+    "snmp_error",
+    "Error metrics",
+    ["name"],
+)
+
+snmp_req_duration = prometheus_client.Summary(
+    "snmp_request_duration",
+    "Time it took for SNMP request",
+)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+logging.basicConfig(
+    # in mondo we trust
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.INFO,
+)
+
+class SnmpOid(enum.Enum):
+    INK_LEVEL = ("ink_level", "1.3.6.1.2.1.43.11.1.1.9.1.1")
+    INK_CAPACITY = ("ink_capacity", "1.3.6.1.2.1.43.11.1.1.8.1.1")
+    PAGE_COUNT = ("page_count", "1.3.6.1.2.1.43.10.2.1.4.1.1")
+    DOOR_STATUS = ("door_status", "1.3.6.1.2.1.43.18.1.1.2.1.12", True)
+    TRAY_STATUS = ("tray_status", "1.3.6.1.2.1.43.18.1.1.2.1.9", True)
+
+    def __init__(self, metric_name, metric_value, is_error=False):
+        self.metric_name = metric_name
+        self.metric_value = metric_value
+        self.is_error = is_error
+
+def get_snmp_data(ip):
+    while True:
+        for oid in SnmpOid:
+            with snmp_req_duration.time():
+                errorIndication, errorStatus, errorIndex, varBinds = next(
+                getCmd(SnmpEngine(),
+                CommunityData('public', mpModel=0),
+                UdpTransportTarget((ip, 161)),
+                ContextData(),
+                ObjectType(ObjectIdentity(oid.metric_value)))
+                )
+                if errorIndication:
+                    logging.error(f"Error: {errorIndication}")
+                elif errorStatus:
+                    logging.error(f"Error: {errorStatus.prettyPrint()}")
+                else:
+                    for res in varBinds:
+                        if oid.is_error:
+                            snmp_error.labels(name=oid.metric_name).set(int(res[1] == 3))
+                            continue
+                        snmp_metric.labels(name=oid.metric_name).set(res[1])
+        time.sleep(args.sleep_duration_minutes * 60)
+
+@app.get("/metrics")
+async def metrics():
+    return Response(
+        content=prometheus_client.generate_latest(),
+        media_type="text/plain",
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("snmp coolness")
+
+    parser.add_argument(
+        "--ip",
+        help="IP address of snmp agent (default: 192.168.69.208)",
+        default="192.168.69.208"
+    )
+    parser.add_argument(
+        "--host",
+        help="ip address to listen for requests on, i.e. 0.0.0.0",
+        default='0.0.0.0',
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        help="port for the server to listen on, default is 5000",
+        default=5000
+    )
+    parser.add_argument(
+        "--sleep-duration-minutes",
+        type=int,
+        help="update sleepy time, default is 2mins",
+        default=2
+    )
+
+    args = parser.parse_args()
+
+    thread = Thread(target = get_snmp_data, args = (args.ip,), daemon=True)
+    thread.start()
+    uvicorn.run(
+        app, 
+        host=args.host, 
+        port=args.port, 
+        # reload=True,
+    )
