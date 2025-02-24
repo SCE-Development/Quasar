764e9bcc01a7890cec612b07738927991dc02cfd
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2025-02-23 17:52:40 -0800

    remove ambiguous name oids

--- collector/server.py
+++ collector/server.py
@@ -52,8 +52,6 @@ class SnmpOid(enum.Enum):
     INK_LEVEL = ("ink_level", "1.3.6.1.2.1.43.11.1.1.9.1.1")
     INK_CAPACITY = ("ink_capacity", "1.3.6.1.2.1.43.11.1.1.8.1.1")
     PAGE_COUNT = ("page_count", "1.3.6.1.2.1.43.10.2.1.4.1.1")
-    DOOR_STATUS = ("door_status", "1.3.6.1.2.1.43.18.1.1.2.1.12", True)
-    TRAY_STATUS = ("tray_status", "1.3.6.1.2.1.43.18.1.1.2.1.9", True)
     TRAY_EMPTY = ("tray_empty", "1.3.6.1.2.1.43.18.1.1.8.1.13", True)
 
     def __init__(self, metric_name, metric_value, is_error=False):
