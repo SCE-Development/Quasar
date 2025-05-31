03622ed474d5e5d41d43e1ebfabb2a6c3c9674ff
Evan Ugarte <36345325+evanugarte@users.noreply.github.com>
2025-05-31 11:08:48 -0700

    Add unit tests to Quasar (#124)
    * Add unit tests to Quasar
    
    * remove -m

---
+++ printer/test_server.py
@@ -0,0 +1,80 @@
+import io
+import os
+import unittest
+from unittest import mock
+
+from fastapi.testclient import TestClient
+from server import app
+
+
+class TestFastAPI(unittest.TestCase):
+    def setUp(self):
+        os.environ["RIGHT_PRINTER_NAME"] = "HP_P2015_DN"
+        self.client = TestClient(app)
+
+    def test_health_check(self):
+        response = self.client.get("/healthcheck/printer")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '"printer is up!"')
+
+    @mock.patch("server.uuid.uuid4", return_value="test-id")
+    @mock.patch("server.subprocess.Popen")
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    @mock.patch("pathlib.Path.unlink")
+    def test_print_endpoint(self, mock_pathlib_unlink, mock_open_func, mock_popen, _):
+        test_file = io.BytesIO(b"dummy file content")
+        response = self.client.post(
+            "/print",
+            files={"file": ("test.txt", test_file, "text/plain")},
+            data={"copies": "1", "sides": "one-sided"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '"worked!"')
+
+        mock_open_func.assert_called_once_with("/tmp/test-id", "wb")
+
+        mock_open_func().write.assert_called_once()
+        self.assertEqual(
+            mock_open_func().write.call_args_list[0], mock.call(b"dummy file content")
+        )
+
+        mock_popen.assert_called_once()
+
+        self.assertEqual(
+            mock_popen.call_args_list[0],
+            mock.call(
+                "lp -n 1  -o sides=one-sided -o media=na_letter_8.5x11in -d HP_P2015_DN /tmp/test-id",
+                shell=True,
+            ),
+        )
+
+        mock_pathlib_unlink.assert_called_once()
+
+    @mock.patch("server.subprocess.Popen")
+    @mock.patch("builtins.open", side_effect=FileNotFoundError("sorry!"))
+    @mock.patch("pathlib.Path.unlink")
+    def test_print_endpoint(self, mock_pathlib_unlink, _, mock_popen):
+        test_file = io.BytesIO(b"dummy file content")
+        response = self.client.post(
+            "/print",
+            files={"file": ("test.txt", test_file, "text/plain")},
+            data={"copies": "1", "sides": "one-sided"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status_code": 500,
+                "detail": "printing failed, check logs",
+                "headers": None,
+            },
+        )
+
+        mock_popen.assert_not_called()
+        mock_pathlib_unlink.assert_not_called()
+
+
+if __name__ == "__main__":
+    unittest.main()
