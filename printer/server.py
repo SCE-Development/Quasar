881f8c0cf0d8607c4f1d7f0e4e260045b8452a2f
epicgdog <91296067+epicgdog@users.noreply.github.com>
2025-06-06 11:56:02 -0700

    from loggin.warning to logging.info

--- printer/server.py
+++ printer/server.py
@@ -118,7 +118,7 @@ def send_file_to_printer(
         print_job.wait()
         if print_job.returncode == 0: # success
             print_id = print_job.stdout.read().strip().split(" ")[3]
-            logging.warning(print_id)
+            logging.info(print_id)
         else: # failed
             logging.error(f"printing failed with error: {print_job.stderr.read()}")
 
