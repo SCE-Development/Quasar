991fdce7a4e670b9b36dc3a16e37ea0a73093ea3
epicgdog <91296067+epicgdog@users.noreply.github.com>
2025-06-06 11:53:09 -0700

    retrieve id from print_job

--- printer/server.py
+++ printer/server.py
@@ -108,13 +108,9 @@ def send_file_to_printer(
         logging.warning(f"server is in development mode, command would've been `{command}`")
     else:
         print_id = ""
-        print_id = ""
         print_job = subprocess.Popen(
             command,
             shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
@@ -122,7 +118,7 @@ def send_file_to_printer(
         print_job.wait()
         if print_job.returncode == 0: # success
             print_id = print_job.stdout.read().strip().split(" ")[3]
-            logging.info(print_id)
+            logging.info(f"extracted print id is {print_id}")
         else: # failed
             logging.error(f"printing failed with error: {print_job.stderr.read()}")
 
