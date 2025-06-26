74be066ce4411a73c6e3d9aa11142dca416bc9cb
epicgdog <91296067+epicgdog@users.noreply.github.com>
2025-06-25 20:46:14 -0700

    added args to popen

--- printer/test_server.py
+++ printer/test_server.py
@@ -1,8 +1,10 @@
 import importlib
 import io
 import os
+import subprocess
 import unittest
 from unittest import mock
+
 
 from fastapi.testclient import TestClient
 
@@ -57,6 +59,9 @@ class TestFastAPI(unittest.TestCase):
             mock.call(
                 "lp -n 1  -o sides=one-sided -o media=na_letter_8.5x11in -d HP_P2015_DN /tmp/test-id",
                 shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
             ),
         )
 
