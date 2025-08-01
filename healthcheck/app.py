da8f3862878d7d4d30281597813ca0fa43b52e63
PotatoDonburi <potatodemon.unity@gmail.com>
2025-07-31 21:39:20 -0700

    return timestamp

--- healthcheck/app.py
+++ healthcheck/app.py
@@ -1,14 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 import requests
 import json
 import sys
 from urllib.parse import urljoin
+import time
+import prometheus_client
 
 app = Flask(__name__)
 
+last_acccess = prometheus_client.Gauge(
+    "last_accessed",
+    "last time accessed",
+)
+
 @app.route("/healthcheck/printer")
 def api():
-    return "printer is up!"
+    metric = time.time()
+    last_acccess.set(metric)
+    return jsonify({"last_accessed": metric})
+    
+@app.route('/metrics')
+def get_metrics():
+    return Response(
+        prometheus_client.generate_latest(),
+        mimetype="text/plain"
+    )
 
 if __name__ == "__main__":
     port = 14000
