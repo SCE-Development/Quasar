4db2acd3284ed8ddf97381e790a0693381fe1151
thebeninator <xccr123@gmail.com>
2025-09-24 13:18:09 -0700

    fix unit test

--- printer/test/test_gerard.py
+++ printer/test/test_gerard.py
@@ -41,6 +41,7 @@ class TestGerard(unittest.TestCase):
             mock_popen.call_args_list[0],
             mock.call(
                 shlex.split(gerard.LP_COMMAND.format(
+                    hold_time="immediate",
                     num_copies=1,
                     maybe_page_range="1",
                     sides="one-side",
@@ -76,6 +77,7 @@ class TestGerard(unittest.TestCase):
             mock_popen.call_args_list[0],
             mock.call(
                 shlex.split(gerard.LP_COMMAND.format(
+                    hold_time="immediate",
                     num_copies=1,
                     maybe_page_range="",
                     sides="dark-side",
@@ -113,6 +115,7 @@ class TestGerard(unittest.TestCase):
             mock_popen.call_args_list[0],
             mock.call(
                 shlex.split(gerard.LP_COMMAND.format(
+                    hold_time="immediate",    
                     num_copies=1,
                     maybe_page_range="1",
                     sides="one-side",
