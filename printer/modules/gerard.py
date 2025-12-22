9a9a331491ee7012d4db7d6c6608445e86486a6b
evan <evanuxd@gmail.com>
2025-12-22 12:54:04 -0800

    is_development_mode

--- printer/modules/gerard.py
+++ printer/modules/gerard.py
@@ -44,7 +44,7 @@ def create_print_job(
     printer_name,
     file_path,
     is_development_mode=False,
-    no_dev_printer=False
+    # no_dev_printer=False
 ):
     hold_time = "immediate"
     if is_development_mode:
@@ -61,7 +61,7 @@ def create_print_job(
         file_path=file_path,
     )
 
-    if no_dev_printer:
+    if is_development_mode:
         logging.warning(
             f"server is in development mode, command would've been `{command}`"
         )
