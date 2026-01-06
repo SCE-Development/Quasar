44fe8c96f8a7c9a97ccca393c37d41a3dca29f50
m3v113 <mehvishbondre@gmail.com>
2026-01-05 21:11:55 -0800

    metric line replaced/removed

--- printer/modules/collector.py
+++ printer/modules/collector.py
@@ -24,7 +24,6 @@ logging.basicConfig(
 
 
 class SnmpOid(enum.Enum):
-    INK_LEVEL = ("ink_level", "1.3.6.1.2.1.43.11.1.1.9.1.1")
     INK_CAPACITY = ("ink_capacity", "1.3.6.1.2.1.43.11.1.1.8.1.1")
     PAGE_COUNT = ("page_count", "1.3.6.1.2.1.43.10.2.1.4.1.1")
     TRAY_EMPTY = ("tray_empty", "1.3.6.1.2.1.43.18.1.1.8.1.13", True)
